af4429a68d0a2ec2b37aefda564623706be4f8ce
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-06-12 12:08:26 +0300

    style JobFiltersSidebar

--- src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96">Job Filters Sidebar</div>
+  <div class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96">
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+       <h3 class="my-4 text-base font-semi-bold"> What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <base-button text="Clear filters" type="secondary"></base-button>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
 export default {
-  name: "JobFiltersSidebar"
+  name: "JobFiltersSidebar",
+  components: {}
+
 };
 </script>
