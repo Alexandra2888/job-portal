dfe5fc5430137732eb94df2e6a2ef74c16538e1b
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-06-12 17:15:34 +0300

    adding JobFilterssidebarOrganization

--- src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
@@ -4,16 +4,24 @@
       <div class="flex flex-row justify-between">
        <h3 class="my-4 text-base font-semi-bold"> What do you want to do?</h3>
         <div class="flex items-center text-sm">
-          <base-button text="Clear filters" type="secondary"></base-button>
+        <base-button type="secondary" text="Clear filters" />
         </div>
       </div>
+      <accordion header="Degree" />
+      <accordion header="Job Type">
+        <p>Placeholder for now</p>
+      </accordion>
+<job-filters-sidebar-organization />
     </section>
   </div>
 </template>
 <script>
+import Accordion from "@/components/shared/Accordion";
+import BaseButton from "@/components/shared/BaseButton";
+import JobFiltersSidebarOrganization from "@/components/jobResults/jobFiltersSidebar/JobFiltersSidebarOrganizations";
 export default {
   name: "JobFiltersSidebar",
-  components: {}
+  components: {JobFiltersSidebarOrganization, BaseButton, Accordion}
 
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import { createApp } from 'vue'
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
 import MainNav from "@/components/navigation/MainNav";
-import store from './store'
+import store from './store';
+import { FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faAngleDown, faAngleUp, faSearch} from "@fortawesome/free-solid-svg-icons";
 
 
 library.add(faSearch);
+library.add(faAngleDown);
+library.add(faAngleUp);
+
 
 const app = createApp(App).use(store);
-app.component("main-nav", MainNav)
+app.component("main-nav", MainNav);
+app.component("font-awesome-icon", FontAwesomeIcon)
 app.use(router);
 app.mount("#app");
 
